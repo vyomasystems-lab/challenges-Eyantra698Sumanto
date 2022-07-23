73decf7f3afc85d8dad9601675f0c3d8617b009c
Sumanto Kar <JEETSUMANTO123@GMAIL.COM>
2022-07-23 23:00:29 +0000

    Add Files

--- level3_design/test_vedic8x8.py
+++ level3_design/test_vedic8x8.py
@@ -5,14 +5,27 @@
 import cocotb
 from cocotb.triggers import Timer
 import random
-from cocotb.decorators import coroutine
-from cocotb.triggers import Timer
 
 @cocotb.test()
 
 @cocotb.test()
 async def vedic8x8_randomised_test(dut):
     """Test for vedic8x8 for random numbers multiple times"""
+    a = random.randint(0, 127)
+    b = random.randint(0, 127)
+    dut.a.value = a
+    dut.b.value = b
+    expected_output = a*b
+    await Timer(2, units='ns')
+    dut_output = dut.prod.value
+    
+    cocotb.log.info(f'Input A = {hex(a)} B = {hex(b)}')
+    cocotb.log.info(f'DUT OUTPUT={hex(dut_output)}')
+    cocotb.log.info(f'EXPECTED OUTPUT={hex(expected_output)}')
+    
+    # comparison
+    error_message = f'Value mismatch DUT = {hex(dut_output)} does not match MODEL = {hex(expected_output)}'
+    assert dut_output == expected_output, error_message
     for i in range(128):
         b = i
         for j in range(128):
